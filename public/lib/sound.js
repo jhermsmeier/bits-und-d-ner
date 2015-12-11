bbb6dec33071ee63032b795168341c8088f3ef8f
Jonas Hermsmeier <jhermsmeier@gmail.com>
2015-12-11 03:17:28 +0100

    Update public: Make links relative
    Enables loading Orbital Cleanup in non-root paths

--- public/lib/sound.js
+++ public/lib/sound.js
@@ -1,7 +1,7 @@
 // No sounds played in debug mode
 
 if ( !DEBUG ) {
-  backgroundMusic = new Audio('../sound/OrbitalCleanup.wav')
+  backgroundMusic = new Audio('sound/OrbitalCleanup.wav')
   backgroundMusic.addEventListener('ended', function() {
       this.currentTime = 0
       this.play()
@@ -9,7 +9,7 @@ if ( !DEBUG ) {
   backgroundMusic.play()
 }
 
-explosionSound = new Audio('../sound/Explosion.wav')
+explosionSound = new Audio('sound/Explosion.wav')
 function playExplosion() {
   if ( DEBUG ) {
     return
@@ -19,7 +19,7 @@ function playExplosion() {
   explosionSound.play()
 }
 
-rubbishSound = new Audio('../sound/Rubbish.wav')
+rubbishSound = new Audio('sound/Rubbish.wav')
 function playRubbish() {
   if ( DEBUG ) {
     return
